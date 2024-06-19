9e9e9271b9e8495e032b12e5fe1bdad0ef7ccf17
rajud123 <63551153+rajud123@users.noreply.github.com>
2024-06-19 15:39:34 +0530

    Create Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,13 @@
+public class Helloworld
+{
+public static void main(int a)  //overloaded main method
+{
+System.out.println(a);
+}
+public static void main(String args[])
+{
+System.out.println("main method incoked");
+main(6);
+}
+}
+~ 
